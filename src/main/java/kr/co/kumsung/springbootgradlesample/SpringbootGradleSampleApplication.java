90db68f5aefef466241d0dda7a0f75a889a3c56a
LAPTOP-UO9MC981\guest1 <woojang@kumsung.co.kr>
2021-10-27 16:15:49 +0900

    first commit

---
+++ src/main/java/kr/co/kumsung/springbootgradlesample/SpringbootGradleSampleApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kumsung.springbootgradlesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGradleSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGradleSampleApplication.class, args);
+	}
+
+}
